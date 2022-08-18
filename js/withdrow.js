6a555f94e19d78ea4bdb17057f98a22c3cd31deb
mehedititu <mh.me.mehedi@gmail.com>
2022-08-18 12:06:37 +0600

    Totally funconal

--- js/withdrow.js
+++ js/withdrow.js
@@ -5,13 +5,23 @@ document.getElementById('withdrow-btn').addEventListener('click', function() {
     const withdrowAmmount = getTextElementID('withdrow-ammount');
     const TotalWithdrow = withdrowAmmount + withdrowBtn;
     
-    // Set total withdrow value 
-    setTheDepositValue ('withdrow-ammount', TotalWithdrow);
+    
 
     // New Ballance after Withdrow
     const currentBallance = getTextElementID('total-ammount');
+
+    if(withdrowBtn>currentBallance) {
+        alert('Your account have less money than $'+ withdrowBtn);
+        return;
+    }
+
+    // Set total withdrow value in the Withrow Ballance
+    setTheDepositValue ('withdrow-ammount', TotalWithdrow);
+
     const newBallance = currentBallance - withdrowBtn;
     setTheDepositValue ('total-ammount', newBallance);
+
+    
 
     
 });
